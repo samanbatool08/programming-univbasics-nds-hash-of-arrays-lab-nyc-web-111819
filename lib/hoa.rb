7b010a8812fbf0ef4b6889db772b106c4e62d315
saman batool <samanbatool08@gmail.com>
2019-11-11 01:24:59 +0000

    Done.

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -4,8 +4,4 @@ BASE_HOA = {
   :jetsons => ["George", "Jane", "Judy", "Elroy"]
 }
 
-
-def add_character(show, name)
-  # Write your implementation here
-  # Should return the array of the 'show' argument
-end
+BASE_HOA [:third_earthers] << "Snarfs"
